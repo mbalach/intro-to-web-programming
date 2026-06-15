88c9df5b2023fcec34fa87b45cf97bf329ba6ab9
mbalach <muhammadbalach.talpur07@gmail.com>
2026-06-15 10:05:00 +0300

    initial-commit

---
+++ foo/bar.js
@@ -0,0 +1,31 @@
+
+
+function printButtonFunc() {
+
+  const printButton = document.getElementById("my-button");
+  const header1 = document.getElementById("header1");
+
+  printButton.addEventListener("click",function() {
+    console.log("Hello World!");
+    let newText = "Moi maailma";
+    header1.innerText = newText;
+  });
+
+}
+
+function addTextFunc() {
+
+  const addButton = document.getElementById("add-button");
+  const myList = document.getElementById("my-list");
+  let itemCounter = 0;
+
+  addButton.addEventListener("click", function () {
+    let newItem = document.createElement("li");
+    newItem.innerText = ++itemCounter + " - " + document.getElementById("message").value;
+    myList.appendChild(newItem);
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", printButtonFunc);
+document.addEventListener("DOMContentLoaded", addTextFunc);
